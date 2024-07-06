5ecd4de78b92604359781f1a7b329e92867c5986
Marlon-Parra <marlonparra765@gmail.com>
2024-07-06 14:11:23 -0500

    nuevo cambio

--- DBS/server.js
+++ DBS/server.js
@@ -1,16 +1,17 @@
 import express from 'express';
 import fs from 'fs/promises';
-import cors from 'cors';
+import cors from 'cors'; // Importa cors
 
 const app = express();
-const port = process.env.PORT || 5000; // Utiliza el puerto proporcionado por Vercel o 5000 por defecto
+const port = process.env.PORT || 5000;
+
 
 app.use(express.json());
-app.use(cors());
+app.use(cors()); // Usa el middleware cors
 
-let videos = [];
+let videos;
 
-// Cargar el archivo JSON usando fs/promises
+// Carga el archivo JSON utilizando fs/promises
 fs.readFile('./dbs.json', 'utf8')
   .then(data => {
     videos = JSON.parse(data);
@@ -42,13 +43,13 @@ fs.readFile('./dbs.json', 'utf8')
     app.put('/api/videos/:id', (req, res) => {
       const videoId = parseInt(req.params.id, 10);
       const updatedVideo = { ...req.body, id: videoId };
-      videos = videos.map(video => (video.id === videoId ? updatedVideo : video));
+      videos = videos.map(video => video.id === videoId ? updatedVideo : video);
       fs.writeFile('./dbs.json', JSON.stringify(videos, null, 2))
         .then(() => res.json(updatedVideo))
         .catch(err => res.status(500).json({ error: 'Error al escribir en el archivo' }));
     });
 
-    // Iniciar el servidor
+    // Inicia el servidor
     app.listen(port, () => {
       console.log(`Server running on port ${port}`);
     });
