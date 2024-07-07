5f7ee3da97d56b2f62760a88ccdae3a245fd0877
Marlon-Parra <marlonparra765@gmail.com>
2024-07-06 23:54:29 -0500

    Actualizar configuración de Vite para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: './', // Configura la base como './' para que las rutas relativas funcionen correctamente
+  plugins: [react()]
+});
