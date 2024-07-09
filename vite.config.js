0129b79f68471968d570e03f24cb193aa656d1c4
Marlon-Parra <marlonparra765@gmail.com>
2024-07-08 21:22:07 -0500

    new aluraflix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // Configura la base como './' para que las rutas relativas funcionen correctamente
-  plugins: [react()]
+  plugins: [react()],
 });
